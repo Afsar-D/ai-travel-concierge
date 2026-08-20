719f2cbab032f54b15bffab1697d384ea123ef71
Afsar-D <afsard2512@gmail.com>
2026-08-20 13:13:39 +0530

    chatbot completed (before integrated with apis)

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit import user
 from streamlit.source_util import Icon
+# sidebar 
 with st.sidebar:
     st.text_input("give me prompt", placeholder='3-days paris trip')
     st.header("Days")
@@ -8,5 +9,23 @@ with st.sidebar:
     st.date_input(label='Date')
     st.header("Budget")
     st.select_slider('Budget', options=['Low', 'Medium', 'High'])
-    
+
+# Main Content
 st.title("Travel Conicerge")
+# st.session_state
+with st.chat_message('ai'):
+    st.text('hello user')
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+if user_input:= st.chat_input(placeholder='Type something here'):
+    st.session_state.messages.append({
+        'role' : 'user',
+        'content' : user_input
+    })
+    st.session_state.messages.append({
+        'role': 'ai',
+        'content' : f'{user_input}, ill look into it'
+    })
+for key in st.session_state.messages:
+    with st.chat_message(key['role']):
+        st.write(key['content'])
