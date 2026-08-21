79b017b6e76c48168ffca7da4dcb7528520541e1
Afsar-D <afsard2512@gmail.com>
2026-08-21 09:10:29 +0530

    streamlit chatbot with gemini api integration

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
 import streamlit as st
 from streamlit import user
 from streamlit.source_util import Icon
+from dotenv import load_dotenv
+from google import genai
+import os
+load_dotenv()
+
 # sidebar 
 with st.sidebar:
     st.text_input("give me prompt", placeholder='3-days paris trip')
@@ -11,21 +17,29 @@ with st.sidebar:
     st.select_slider('Budget', options=['Low', 'Medium', 'High'])
 
 # Main Content
+## API Integration
+api = os.getenv('GEMINI_API_KEY')
+client = genai.Client(api_key=api)
+
 st.title("Travel Conicerge")
 # st.session_state
 with st.chat_message('ai'):
     st.text('hello user')
 if 'messages' not in st.session_state:
     st.session_state.messages = []
+
 if user_input:= st.chat_input(placeholder='Type something here'):
     st.session_state.messages.append({
         'role' : 'user',
         'content' : user_input
     })
+    response = client.models.generate_content(model='gemini-3.5-flash-lite',contents=user_input)
     st.session_state.messages.append({
         'role': 'ai',
-        'content' : f'{user_input}, ill look into it'
+        'content' : f'{response.text}'
     })
+
 for key in st.session_state.messages:
     with st.chat_message(key['role']):
         st.write(key['content'])
+
